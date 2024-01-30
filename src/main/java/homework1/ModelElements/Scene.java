25c18b8989e38fa02d21d78b00ca8e6c9faf106a
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-30 22:51:28 +0700

    attributes definition is done

--- src/main/java/homework1/ModelElements/Scene.java
+++ src/main/java/homework1/ModelElements/Scene.java
@@ -1,4 +1,14 @@
 package homework1.ModelElements;
 
+import java.util.List;
+
 public class Scene {
+    public Integer Id;
+    public List<PoligonalModel> Models;
+    public List<Flash> Flashes;
+    public List<Camera> Cameras;
+
+    public Scene() {
+
+    }
 }
