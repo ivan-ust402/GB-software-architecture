8eea252ca0592282b14009d4b378e87666aa9526
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-30 22:40:08 +0700

    Flash class filled

--- src/main/java/homework1/ModelElements/Flash.java
+++ src/main/java/homework1/ModelElements/Flash.java
@@ -2,10 +2,16 @@ package homework1.ModelElements;
 
 import homework1.Stuff.Angle3D;
 import homework1.Stuff.Point3D;
+import homework1.Stuff.Color;
+import homework1.Stuff.Float;
 
 public class Flash {
     public Point3D Location;
     public Angle3D Angle;
     public Color Color;
+    public Float Power;
 
+    public void Rotate(Angle3D angle) {}
+
+    public void Move(Point3D location) {}
 }
