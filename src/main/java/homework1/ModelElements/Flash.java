69f21a4ff0013b438a1a7c0bfca0c9735c1ca08c
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-30 22:33:43 +0700

    Add Color and Float class

--- src/main/java/homework1/ModelElements/Flash.java
+++ src/main/java/homework1/ModelElements/Flash.java
@@ -1,4 +1,11 @@
 package homework1.ModelElements;
 
+import homework1.Stuff.Angle3D;
+import homework1.Stuff.Point3D;
+
 public class Flash {
+    public Point3D Location;
+    public Angle3D Angle;
+    public Color Color;
+
 }
