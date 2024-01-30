99c0045347983d3f2d70b56edb416b2c5fe4f38f
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-30 22:43:33 +0700

    Camera class is filled

--- src/main/java/homework1/ModelElements/Camera.java
+++ src/main/java/homework1/ModelElements/Camera.java
@@ -1,4 +1,13 @@
 package homework1.ModelElements;
 
+import homework1.Stuff.Angle3D;
+import homework1.Stuff.Point3D;
+
 public class Camera {
+    public Point3D Location;
+    public Angle3D Angle;
+
+    public void Rotate(Angle3D angle){};
+    public void Move(Point3D location){};
+
 }
