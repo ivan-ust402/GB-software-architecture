a40bf35ca90224444dd249b4ca1610e53319427f
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-30 22:22:55 +0700

    Poligon class filled

--- src/main/java/homework1/ModelElements/Poligon.java
+++ src/main/java/homework1/ModelElements/Poligon.java
@@ -1,4 +1,14 @@
 package homework1.ModelElements;
 
+import homework1.Stuff.Point3D;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Poligon {
+    public List<Point3D> Points = new ArrayList<>();
+
+    public Poligon(List<Point3D> Points) {
+        this.Points = Points;
+    }
 }
