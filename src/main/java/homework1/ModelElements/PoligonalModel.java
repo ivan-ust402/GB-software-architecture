e5f3e7f8a02798089f4e97652797fba89c5d1ad5
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-30 22:29:34 +0700

    PoligonalModel class filled

--- src/main/java/homework1/ModelElements/PoligonalModel.java
+++ src/main/java/homework1/ModelElements/PoligonalModel.java
@@ -1,6 +1,17 @@
 package homework1.ModelElements;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PoligonalModel {
+    public List<Poligon> Poligons;
+    public List<Texture> Textures;
+
+    public PoligonalModel(List<Texture> Textures) {
+        this.Poligons = new ArrayList<>();
+        this.Textures = Textures;
+
+        Poligons.add(new Poligon(null));
+    }
+
 }
