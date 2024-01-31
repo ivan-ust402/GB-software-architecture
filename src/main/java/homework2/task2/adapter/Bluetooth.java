15163aea7fc65289087352b8d879d23ec488d7e7
Ivan Ustyantsev <vangold915@gmail.com>
2024-01-31 19:55:46 +0700

    Created Computer classwith two extra classes: Usb and Bluetooth

---
+++ src/main/java/homework2/task2/adapter/Bluetooth.java
@@ -0,0 +1,4 @@
+package homework2.task2.adapter;
+
+public class Bluetooth {
+}
---
+++ src/main/java/homework2/task2/adapter/Computer.java
@@ -0,0 +1,10 @@
+package homework2.task2.adapter;
+
+public class Computer {
+    public void accept(Bluetooth bluetooth) {
+
+    }
+    public void accept(Usb usb) {
+
+    }
+}
