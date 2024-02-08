462ef80597ebdb17e6d5361d0970fd14f832954e
Ivan Ustyantsev <vangold915@gmail.com>
2024-02-08 15:55:00 +0700

    event class created

---
+++ src/main/java/homework4/Event.java
@@ -0,0 +1,25 @@
+package homework4;
+
+import java.util.Date;
+
+public class Event {
+    private int eventId;
+    private Date date;
+    private Vanue venue;
+    private int ticketsAvailable;
+
+
+    public Event(int eventId, Date date, Vanue venue, int ticketsAvailable) {
+        this.eventId = eventId;
+        this.date = date;
+        this.venue = venue;
+        this.ticketsAvailable = ticketsAvailable;
+    }
+
+    public void getDetails() {
+        System.out.println("Id мероприятия: " + eventId
+                + "Дата мероприятия: " + date
+                + "Место проведения: " + venue
+                + "Число доступных билетов: " + ticketsAvailable);
+    }
+}
