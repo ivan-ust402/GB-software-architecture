7f45d56342d1ce013045eff1540a407d9bff6862
Ivan Ustyantsev <vangold915@gmail.com>
2024-02-08 20:59:09 +0700

    Add some comments to the EventProvider class

--- src/main/java/homework4/EventProvider.java
+++ src/main/java/homework4/EventProvider.java
@@ -10,16 +10,25 @@ import static java.util.UUID.randomUUID;
 public class EventProvider {
     private ArrayList<Event> events;
 
+    /**
+     * метод создания мероприятия
+     * */
     public Event createEvent(String name, Date date, Venue venue, int ticketsAvailable) {
         Event newEvent = new Event(randomUUID(), date, venue, ticketsAvailable);
         events.add(newEvent);
         return newEvent;
     }
 
+    /**
+     * метод получения всех существующих мероприятий
+     * */
     public ArrayList<Event> getEvents() {
         return events;
     }
 
+    /**
+     * метод получения мероприятия
+     * */
     public Event getEvent(Date date, Venue venue) {
         for (Event item : events) {
             if (Objects.equals(item.date, date)
