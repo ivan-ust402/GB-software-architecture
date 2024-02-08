68440be8bd1bb9e0205dc2c90da58889a4a08176
Ivan Ustyantsev <vangold915@gmail.com>
2024-02-08 20:56:58 +0700

    Add some comments to the TicketProvider class

--- src/main/java/homework4/TicketProvider.java
+++ src/main/java/homework4/TicketProvider.java
@@ -8,16 +8,25 @@ import static java.util.UUID.randomUUID;
 public class TicketProvider {
     private ArrayList<Ticket> tickets;
 
+    /**
+     * метод получения массива билетов
+     * */
     public ArrayList<Ticket> getTickets() {
         return tickets;
     }
 
+    /**
+     * метод продажи и покупки билета
+     * */
     public Ticket sellTicket(Event event,
                              Customer customer,
                              CashProvider cashProvider) {
         return new Ticket(randomUUID(), 10000, event);
     }
 
+    /**
+     * метод обновления статуса билета
+     * */
     public boolean updateTicketStatus(Ticket ticket) {
         return true;
     }
